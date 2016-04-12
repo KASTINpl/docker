be2acc310e86a4751c0be1c2f5b2d5e50c38b7d0
KASTINpl <kastin@kastin.pl>
2016-04-12 18:15:10 +0200

    CMD

--- php56-fpm/entrypoint.sh
+++ php56-fpm/entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-set -e
 
-OPT_UID=`stat -c "%u" /var/www`
+OPT_UID=`stat -c "%u" /opt`;
 
 if (( $OPT_UID > 0 )); then
-    usermod -u $OPT_UID www-data
+    usermod -u $OPT_UID www-data;
 fi
 
 exec "$@"
