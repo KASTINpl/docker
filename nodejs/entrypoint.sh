057da2c4195e8429655c452434a3736a4dca098e
KASTIN.pl <kastin@kastin.pl>
2016-04-26 07:11:13 +0200

    Update entrypoint.sh

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -5,7 +5,7 @@ OPT_UID=`stat -c "%u" /opt`
 if [ "$OPT_UID" -gt "0" ]; then
     usermod -u $OPT_UID nodejs
     chown nodejs:nodejs -R /home/nodejs
-    sudo -u nodejs $@
-else
-    exec "$@"
+    su nodejs
 fi
+
+exec "$@"
